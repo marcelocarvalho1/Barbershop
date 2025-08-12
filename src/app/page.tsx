ef456b97e85cba40f375fd9ac16ff4631aeda38d
Marcelo <marceloo.dev@gmail.com>
2025-08-11 21:42:37 -0300

    chore: Add Logo, Cabeçalho, Input and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
-import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 //todo components são server components
 export default function Home() {
-  return <Button> Teste </Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Marcelo</h2>
+        <p> Segunda-Feira, 11 de Agosto </p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Pesquisar..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative w-full h-[150px] mt-6 ">
+          <Image alt="banner" src="/BannerPizza.png" fill className="object-cover rounded-xl" />
+        </div>
+      </div>
+    </div>
+  )
 }
