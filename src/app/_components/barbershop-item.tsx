4c0b816e9425e10ce6d4c0a788eda0e2adfe06c1
Marcelo <marceloo.dev@gmail.com>
2025-08-16 00:40:53 -0300

    add recommend barbershop

--- src/app/_components/barbershop-item.tsx
+++ src/app/_components/barbershop-item.tsx
@@ -1,8 +1,36 @@
-const BarberShopItem = ({barbershop}) => {
-    return (  
-        <h1> {barbershop.name}</h1>
+import { Barbershop } from "@prisma/client"
+import { Card, CardContent } from "./ui/card"
+import Image from "next/image"
+import { Button } from "./ui/button"
 
-    );
+interface BarbershopItemProps {
+  barbershop: Barbershop
 }
- 
-export default BarberShopItem;
+const BarberShopItem = ({ barbershop }: BarbershopItemProps) => {
+  return (
+    <Card className="min-w-[167px] rounded-2xl">
+      <CardContent className="p-0 px-1 pt-1 pb-3">
+        {/* IMAGEM */}
+        <div className="relative h-[159px] w-full">
+          <Image
+            alt={barbershop.name}
+            fill
+            className="object-cover rounded-2xl"
+            src={barbershop.imageUrl}
+          />
+        </div>
+        {/* TEXTO */}
+        <div className="px-1 py-3">
+          <h3 className="truncate font-semibold">{barbershop.name}</h3>
+          <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
+          <Button variant="secondary" className="mt-3 w-full">
+            {" "}
+            Reservar{" "}
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default BarberShopItem
